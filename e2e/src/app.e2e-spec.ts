c07e049c090a4d7c2cb60496e94de20f375a2b0e
Rafa <raf_ws@hotmail.com>
2019-11-18 10:34:12 -0300

    express

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Civilization app is running!');
+    expect(page.getTitleText()).toEqual('evolution app is running!');
   });
 
   afterEach(async () => {
